aef461cdee2ba90cb585cf95cf12d4b315f073a3
ahf <1285819042@qq.com>
2021-05-02 13:39:32 +0800

    feat: 增加iframe数据大屏

--- src/pages/big-screen/index.tsx
+++ src/pages/big-screen/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const BigScreen = () => {
-    return (
-        <div>
-            数据大屏
-        </div>
-    )
-}
+  return (
+    <iframe
+      src="http://ncov.leafcoder.cn/demo/"
+      title="数据大屏"
+      width="100%"
+      height="100%"
+    ></iframe>
+  );
+};
 
-export default BigScreen
+export default BigScreen;
